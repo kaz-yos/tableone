e44da9c70bfb9b891e2451bb6f13cbc9fc095d65
kaz-yos <kazky@mac.com>
2014-01-22 01:37:20 -0500

    CreateContTable with by/sapply/sapply loop

--- R/CreateContTable.R
+++ R/CreateContTable.R
@@ -1,100 +1,102 @@
 ## A function to create a table for continuous variables
+## Modification of "descriptive.table.R" in Deducer version 0.7-6.1 published on 2013-10-28 by Ian Fellows et al.
+
 CreateContTable <- function(vars,       # vector of characters
                             strata,     # single element character vector
                             data,       # data frame
                             func.names = c(
-                                "N","Miss",
-                                "Mean","SD",
-                                "Median","Q25","Q75","Min","Max",
-                                "Skew","Kurt"
+                                "n","miss",
+                                "mean","sd",
+                                "median","q25","q75","min","max",
+                                "skew","kurt"
                                 ),
                             func.additional) {
 
     ## Extract necessary variables
-    ## dat <- eval(substitute(vars), data, parent.frame())
     dat <- data[c(vars)]
 
-    ## if(length(dim(dat))<1.5)
-    ##     dat <- d(dat)
-
+    ## Handle non-numeric elements
     if(any(!sapply(dat, is.numeric))){
         ## If there is any non-numeric variables
         dat <- dat[sapply(dat, is.numeric)]
         warning("Non-numeric variables dropped")
     }
+
+    ## Condition on the presence/absence of the strata
     if(missing(strata)){
-        ## If there is no strata
+        ## If there is no strata, name the list "Overall"
         strata <- rep("Overall", dim(dat)[1])
-    }else{
+        
+    } else {
         ## Extract the stratifying variable vector
-        ## strata <- eval(substitute(strata), data, parent.frame())
         strata <- data[c(strata)]
-        ## if(length(dim(strata))<1.5)
-        ##     strata <- d(strata)
     }
 
-    ## Check if all the variables are continuous
-    if(!all(sapply(dat, is.numeric))) stop("Can only be run on numeric variables")
+    ## Check if all the variables are continuous, and stop if not
+    if(!all(sapply(dat, is.numeric))) {stop("Can only be run on numeric variables")}
 
-    ## Create indexes for default variables
-    func.indexes <- pmatch(func.names, c("N","Miss",
-                                         "Mean","SD",
-                                         "Median","Q25","Q75","Min","Max",
-                                         "Skew","Kurt"))
+    ## Create indexes for default variables by partial string matching with the func.names argument
+    func.indexes <- pmatch(func.names, c("n","miss",
+                                         "mean","sd",
+                                         "median","q25","q75","min","max",
+                                         "skew","kurt"))
+    ## Remove NA
+    func.indexes <- func.indexes[!is.na(func.indexes)]
+    
 
     ## Create a list of default functions
-    functions <- c(function(x) length(x),
-                   function(x) sum(is.na(x)),
-                   function(x) mean(x, na.rm = TRUE),
-                   function(x) sd(x, na.rm = TRUE),
-                   function(x) median(x, na.rm = TRUE),
-                   function(x) quantile(x, probs = 0.25, na.rm = TRUE),
-                   function(x) quantile(x, probs = 0.75, na.rm = TRUE),
-                   function(x) min(x, na.rm = TRUE),
-                   function(x) max(x, na.rm = TRUE),
-                   function(x) skewness(x, na.rm = TRUE, type = 2),     # type 2 as in SAS and SPSS
-                   function(x) kurtosis(x, na.rm = TRUE, type = 2)      # type 2 as in SAS and SPSS
+    functions <- c("n"      = function(x) length(x),
+                   "miss"   = function(x) sum(is.na(x)),
+                   "mean"   = function(x) mean(x, na.rm = TRUE),
+                   "sd"     = function(x) sd(x, na.rm = TRUE),
+                   "median" = function(x) median(x, na.rm = TRUE),
+                   "q25"    = function(x) quantile(x, probs = 0.25, na.rm = TRUE),
+                   "q75"    = function(x) quantile(x, probs = 0.75, na.rm = TRUE),
+                   "min"    = function(x) min(x, na.rm = TRUE),
+                   "max"    = function(x) max(x, na.rm = TRUE),
+                   "skew"   = function(x) skewness(x, na.rm = TRUE, type = 2),     # type 2 as in SAS and SPSS
+                   "kurt"   = function(x) kurtosis(x, na.rm = TRUE, type = 2)      # type 2 as in SAS and SPSS
                    )
-
+    
+    ## Keep only functions in use
     functions <- functions[func.indexes]
 
+    ## Check for additional functions
     if(!missing(func.additional)){
         ## When additional functions are given
-        if(!is.list(func.additional) || is.null(names(func.additional)))
-
+        if(!is.list(func.additional) || is.null(names(func.additional))) {
+            ## Stop if not a named list
             stop("func.additional must be a named list of functions")
+        }
+
+        ## If a named list is given, add to the vector of functions and their names
         functions  <- c(functions, unlist(func.additional))
         func.names <- c(func.names, names(func.additional))
     }
 
-    ## calculate statistics
-    tbl.list <- list()
+    
+    ## strata-functions-variable structure alternative 2014-01-22
 
-    for(ind in 1:length(functions)){
-        tbl.list[[func.names[ind]]] <- by(dat, strata,
-                                          function(x) sapply(x, functions[[ind]]) , simplify = FALSE)
-    }
-    ## format into table
-    result <- list()
+    ## Devide by strata
+    result <- by(data = dat, INDICES = strata,
 
-    ## for(ind in 1:length(tbl.list[[1]])){
-    for(ind in seq_along(tbl.list[[1]])){
+                 ## Work on each stratum
+                 FUN = function(strataDat) { # strataDat should be a data frame
 
-        d <- dim(tbl.list[[1]])
-        dn <- dimnames(tbl.list[[1]])
-        dnn <- names(dn)
-        ii <- ind - 1
-        name <- ""
-        for (j in seq_along(dn)) {
-            iii <- ii%%d[j] + 1
-            ii <- ii%/%d[j]
-            name <- paste(name, dnn[j], ":", dn[[j]][iii], " ", sep = "")
-        }
-        result[[name]]<-sapply(tbl.list,function(x) x[[ind]])
-    }
+                     ## Loop for functions
+                     sapply(functions,
+                            FUN = function(fun) {
 
+                                ## Loop for variables
+                                sapply(strataDat, fun)
+                            })
+                 })
+
+    ## Not sure what this does.
     ## clean out nulls
-    result <- result[!sapply(result, function(x) all(sapply(x,function(x)all(is.null(x)))))]
+    ## Drop strata with any null values?
+    ## result <- result[!sapply(result, # loop on strata
+    ##                          function(x) {all(sapply(x, function(x) {all(is.null(x))}))})]
 
     ## Give an S3 class
     class(result) <- "ContTable"
