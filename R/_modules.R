4589c82593b26fb1e4c4cafa87fee96d6f4825ef
kaz-yos <kazky@mac.com>
2014-02-10 07:22:27 -0500

    modules file created (empty)

---
+++ R/_modules.R
@@ -0,0 +1,8 @@
+################################################################################
+### This file contains small modules used in the tableone package.
+## Modules were created from common parts shared by multiple functions.
+## Created on: 2014-02-10
+## Author: Kazuki Yoshida
+################################################################################
+
+
