c1956021be812da317d0fae669ab4447be0c2021
kaz-yos <kazky@mac.com>
2014-02-11 19:02:24 -0500

    seealso updated for package description

--- R/tableone-package.R
+++ R/tableone-package.R
@@ -1,12 +1,12 @@
 ##' Create "Table 1" to describe baseline characteristics
-##' 
+##'
 ##' Table 1, i.e., description of baseline patient characteristics is essential
 ##' every medical research. This package provides functions to create such
 ##' summaries for continuous and categorical variables, optionally with subgroups
 ##' and groupwise comparison. The package was insipired by descriptive statistics
 ##' functions in Deducer, Java-based GUI package. This package does not require
 ##' GUI or Java, and intended for CUI users.
-##' 
+##'
 ##' \tabular{ll}{ Package: \tab tableone\cr Type: \tab Package\cr Version: \tab
 ##' 0.1.3\cr Date: \tab 2014-02-08\cr License: \tab GPL-2\cr } Create an object
 ##' summarizing continous and categorical variables optionally stratifying by
@@ -14,52 +14,52 @@
 ##' object gives a table that is easy to use in medical research papers.
 ##' Continuous variables are handled by CreateContTable, and categorical
 ##' variables are handled by CreateCatTable.
-##' 
+##'
 ##' @name tableone-package
 ##' @aliases tableone-package tableone
 ##' @docType package
 ##' @note Special Thanks:
 ##' Ian Fellows for developing the Deducer package, which this package is based on.
 ##' Hadley Wickham for packaging advice and for creating tools this package was made with.
-##' 
+##'
 ##' Developmental repository is on github. Your contributions are appreciated.
 ##' https://github.com/kaz-yos/tableone
-##' 
+##'
 ##' @author Kazuki Yoshida, Justin Bohn
-##' 
+##'
 ##' Maintainer: Kazuki Yoshida <kazukiyoshida@@mail.harvard.edu>
-##' @seealso \code{\link{CreateCatTable}}, \code{\link{print.CatTable}},
-##' \code{\link{summary.CatTable}}, \code{\link{CreateContTable}}, \code{\link{print.ContTable}},
-##' \code{\link{summary.ContTable}}
-## @references
-## @keywords package
+##' @seealso
+##' \code{\link{CreateTableOne}}, \code{\link{print.TableOne}}, \code{\link{summary.TableOne}},
+##' \code{\link{CreateContTable}}, \code{\link{print.ContTable}}, \code{\link{summary.ContTable}},
+##' \code{\link{CreateCatTable}}, \code{\link{print.CatTable}}, \code{\link{summary.CatTable}},
+##' \code{\link{ShowRegTable}}
 ##' @examples
-##' 
+##'
 ##' ## Load
 ##' library(tableone)
-##' 
+##'
 ##' ## Load Mayo Clinic Primary Biliary Cirrhosis Data
 ##' library(survival)
 ##' data(pbc)
 ##' ## Check variables
 ##' head(pbc)
-##' 
+##'
 ##' ## Create an overall table for categorical variables
 ##' catVars <- c("status","ascites","hepato","spiders","edema","stage")
 ##' catTableOverall <- CreateCatTable(vars = catVars, data = pbc)
-##' 
+##'
 ##' ## Simply typing the object name will invoke the print.CatTable method,
 ##' ## which will show the sample size, frequencies and percentages.
 ##' ## For 2-level variables, only the higher level is shown for simplicity.
 ##' catTableOverall
-##' 
+##'
 ##' ## Create an overall table for continuous variables
 ##' contVars <- c("time","age","bili","chol","albumin","copper","alk.phos","ast",
 ##'               "trig","platelet","protime")
 ##' contTableOverall <- CreateContTable(vars = contVars, data = pbc)
-##' 
+##'
 ##' ## Simply typing the object name will invoke the print.ContTable method,
 ##' ## which will show the sample size, means and standard deviations.
 ##' contTableOverall
-##' 
+##'
 NULL
