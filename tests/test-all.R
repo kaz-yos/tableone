1bb9fb54201a42b71400b6de94f9ee28d2174c8d
kaz-yos <kazky@mac.com>
2014-06-01 12:53:49 -0400

    unit test files added

---
+++ tests/test-all.R
@@ -0,0 +1,12 @@
+################################################################################
+### Unit tests for the modules
+## Reference: http://adv-r.had.co.nz/Testing.html
+## Created on: 2014-06-01
+## Author: Kazuki Yoshida
+################################################################################
+
+
+### Prepare environment
+################################################################################
+library(testthat)
+test_check("tableone")
---
+++ tests/testthat/test-modules.R
@@ -0,0 +1,14 @@
+################################################################################
+### Unit tests for the modules
+## Reference: http://adv-r.had.co.nz/Testing.html
+## Created on: 2014-06-01
+## Author: Kazuki Yoshida
+################################################################################
+
+
+### Prepare environment
+################################################################################
+
+
+### 
+cat("Running unit tests for the modules\n")
