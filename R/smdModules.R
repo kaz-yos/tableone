fa3e0739b1b2ff39afb37cbe9ff87b187403bd0c
kaz-yos <kazukiyoshida@mail.harvard.edu>
2015-07-29 16:19:16 -0400

    Add initial functions for standardize mean differences (passing)
    Continuous/binary functions
    Categorical (including binary) functions
    for both unweighted and weighted

---
+++ R/smdModules.R
@@ -0,0 +1,215 @@
+################################################################################
+### Modules for standardized mean difference support
+##
+## Created on: 2015-07-29
+## Author: Kazuki Yoshida
+################################################################################
+
+###
+### References
+################################################################################
+## A unified approach to measuring the effect size between two groups using SAS®
+## http://support.sas.com/resources/papers/proceedings12/335-2012.pdf
+
+
+###
+### Helpers common to both
+################################################################################
+
+## Multinomial standardized differences
+## A unified approach to measuring the effect size between two groups using SAS®
+## http://support.sas.com/resources/papers/proceedings12/335-2012.pdf
+## R multinomial distribution variance
+## http://stackoverflow.com/questions/19960605/r-multinomial-distribution-variance
+## Receives dummie variable matrix with no redundant column
+
+## Dummy matrix to means (proportions for each dummy)
+MultinomialMeans <- function(dummyMat, na.rm = TRUE) {
+    colMeans(dummyMat, na.rm = na.rm)
+}
+
+## Proportions to variance-covariance matrix
+MultinomialVar <- function(multinomialMeans) {
+    ## Mean for each
+    p <- multinomialMeans
+    ## Diagonal elements are p_i(1-p_i)
+    vars <- p * (1 - p)
+    ## Off-diagonal elements are - p_i p_j where i != j
+    covs <- - outer(p, p)
+    ## Put diagonal elements
+    diag(covs) <- vars
+    ## Reduce to a scalar if 1x1
+    drop(covs)
+}
+
+##
+StdDiffFromLstMeans <- function(lstMeans) {
+
+    ## list of covariance matrices
+    lstCovs  <- lapply(lstMeans,   MultinomialVar)
+
+    ## All possible mean vector differences
+    lstMeanDiffs <- lapply(lstMeans, function(x) {
+        lapply(lstMeans, function(y) {
+            x - y
+        })
+    })
+
+    ## All possible covariance matrix means
+    lstCovMeans <- lapply(lstCovs, function(x) {
+        lapply(lstCovs, function(y) {
+            (x + y) / 2
+        })
+    })
+
+    ## Initialize a numeric vector object for capturing values
+    smds <- vector(mode = "numeric")
+
+    ## Add upper triangle elements (i < j) to output list
+    for (i in seq_along(lstMeans)) {
+        for (j in seq_along(lstMeans)) {
+            if (i < j) {
+                ## For upper triangle elements only
+                ## Squared Mahalanobis distance
+                ## meanDiffs^T covMean^-1 meanDiffs
+                sqMahaDist <-
+                t(lstMeanDiffs[[i]][[j]]) %*%
+                MASS::ginv(lstCovMeans[[i]][[j]]) %*%
+                t(t(lstMeanDiffs[[i]][[j]]))
+                ## Add sqrt of MD to output
+                ## Not efficient; room for improvement
+                smds <- c(smds, sqrt(sqMahaDist))
+            }
+        }
+    }
+    smds
+}
+
+
+## Given strata x levels of variable table
+## Return list of proportions of levels dropping redundant first level
+LstMeansFromFullTable <- function(strataByLevels) {
+    ## Proportion within each stratum
+    ## Equivalent to mean of dummy variables
+    propTables <- prop.table(strataByLevels, margin = 1)
+    ## Drop first level to eliminate dependence
+    propTables <- propTables[, -1, drop = FALSE]
+
+    ## list of mean vectors (Missing values are discarded)
+    lstMeans <- lapply(seq_len(nrow(propTables)), function(i) {
+
+        propTables[i,]
+    })
+    lstMeans
+}
+
+
+
+
+
+###
+### Functions for unweighted data only
+################################################################################
+
+### Continuous/binary standardized mean differences
+## Expects continuous or 0,1 binary variable
+StdDiff <- function(variable, group, binary = FALSE, na.rm = TRUE) {
+
+    ## Proportion of 1 is the mean of variable
+    means <- tapply(variable, group, mean, na.rm = na.rm)
+
+    ## Variance is p(1-p)
+    if (binary) {
+        vars  <- means * (1 - means)
+    } else {
+        vars  <- tapply(variable, group, var, na.rm = na.rm)
+    }
+
+    ## Outer to obtain all pairwise differences
+    meanDiffs  <- outer(X = means, Y = means, FUN = "-")
+    ## Outer to obtain all pairwise variance mean
+    varMeans   <- outer(X = vars, Y = vars, FUN = "+") / 2
+
+    out <- meanDiffs / sqrt(varMeans)
+
+    abs(out[upper.tri(out)])
+}
+
+### Categorical (including binary) standardizzed mean difference
+StdDiffMulti <- function(variable, group) {
+
+    ## strata x variable table
+    strataByLevels <- table(group, variable)
+    lstMeans       <- LstMeansFromFullTable(strataByLevels)
+    ## Return vector of SMDs
+    StdDiffFromLstMeans(lstMeans)
+}
+
+
+### Standardized mean differences for multiple variables
+## Continuous or binary only
+StdDiffs <- function(data, vars, groupVar, binaryVars) {
+
+    lapply(vars, function(var) {
+
+        StdDiff(variable = data[,var], group = data[,groupVar],
+                binary = (var %in% binaryVars))
+    })
+}
+
+
+###
+### Functions for weighted data only
+################################################################################
+
+### Continuous/binary standardized mean differences
+## Expects continuous or 0,1 binary variable
+svyStdDiff <- function(varName, groupName, design, binary = FALSE, na.rm = TRUE) {
+
+    varFormula   <- as.formula(paste("~", varName))
+    groupFormula <- as.formula(paste("~", groupName))
+
+    means <- svyby(formula = varFormula, by = groupFormula,
+                   FUN = svymean, design = design, na.rm = na.rm)[,2]
+
+    if (binary) {
+        vars  <- means * (1 - means)
+    } else {
+        vars  <- svyby(formula = varFormula, by = groupFormula,
+                       FUN = svyvar,  design = design, na.rm = na.rm)[,2]
+    }
+
+    ## Outer to obtain all pairwise differences
+    meanDiffs <- outer(X = means, Y = means, FUN = "-")
+    ## Outer to obtain all pairwise variance mean
+    varMeans  <- outer(X = vars, Y = vars, FUN = "+") / 2
+
+    out <- meanDiffs / sqrt(varMeans)
+
+    abs(out[upper.tri(out)])
+}
+
+
+### Categorical (including binary) standardizzed mean difference
+svyStdDiffMulti <- function(varName, groupName, design) {
+
+    tabFormula   <- as.formula(sprintf("~ %s + %s", groupName, varName))
+
+    ## strata x variable table
+    strataByLevels <- svytable(formula = tabFormula, design = design)
+    lstMeans       <- LstMeansFromFullTable(strataByLevels)
+    ## Return vector of SMDs
+    StdDiffFromLstMeans(lstMeans)
+}
+
+
+### Standardized mean differences for multiple variables
+## Continuous or binary only
+svyStdDiffs <- function(data, vars, groupVar, binaryVars) {
+
+    lapply(vars, function(var) {
+
+        svyStdDiff(varName = var, groupName = groupVar, design = data,
+                   binary = (var %in% binaryVars))
+    })
+}
---
+++ tests/testthat/test-smdModules.R
@@ -0,0 +1,274 @@
+################################################################################
+### Unit tests for the standardized mean difference modules
+## Reference: http://adv-r.had.co.nz/Testing.html
+## Created on:
+## Author: Kazuki Yoshida
+################################################################################
+
+### Structure
+## expectations within tests within context
+
+
+###
+### References
+################################################################################
+## A unified approach to measuring the effect size between two groups using SAS®
+## http://support.sas.com/resources/papers/proceedings12/335-2012.pdf
+
+
+###
+### Prepare environment
+################################################################################
+library(testthat)
+library(survey)
+
+
+###
+### Actual tests
+################################################################################
+context("Tests for functions for standardized mean differences")
+
+
+### Old functions explicitly for 3 groups
+## Standardize differences
+StdDiffOld <- function(variable, group) {
+    ## For each group
+    means <- tapply(variable, group, mean)
+    vars  <- tapply(variable, group, var)
+
+    ## Calculate for all three possible pairs
+    g1g2 <- abs(means[1] - means[2]) / sqrt((vars[1] + vars[2]) / 2)
+    g2g3 <- abs(means[2] - means[3]) / sqrt((vars[2] + vars[3]) / 2)
+    g3g1 <- abs(means[3] - means[1]) / sqrt((vars[3] + vars[1]) / 2)
+
+    ## 2vs1, 3vs1, then 3vs2 to be consistent with regression
+    out <- c(g1g2, g3g1, g2g3)
+    names(out) <- c("3vs2","3vs1","2vs1")
+    out
+}
+
+svyStdDiffOld <- function(varName, groupName, design) {
+
+    varFormula   <- as.formula(paste("~", varName))
+    groupFormula <- as.formula(paste("~", groupName))
+
+    means <- svyby(formula = varFormula, by = groupFormula, FUN = svymean, design = design)[,2]
+    vars  <- svyby(formula = varFormula, by = groupFormula, FUN = svyvar,  design = design)[,2]
+
+    ## Calculate for all three possible pairs
+    g1g2 <- abs(means[1] - means[2]) / sqrt((vars[1] + vars[2]) / 2)
+    g2g3 <- abs(means[2] - means[3]) / sqrt((vars[2] + vars[3]) / 2)
+    g3g1 <- abs(means[3] - means[1]) / sqrt((vars[3] + vars[1]) / 2)
+
+    ## 2vs1, 3vs1, then 3vs2 to be consistent with regression
+    out <- c(g1g2, g3g1, g2g3)
+    names(out) <- c("3vs2","3vs1","2vs1")
+    out
+}
+
+
+test_that("binary standardized difference is correct", {
+
+    ## Prepare data
+    variable <- c(1,0,0,0,0, 1,1,0,0,0, 1,1,1,0,0)
+    group    <- rep(1:3, each = 5)
+
+    data1 <- data.frame(variable = variable, group = group,
+                        weight = rep(0.5, 15))
+    data1Svy <- svydesign(ids = ~ 1, data = data1, weights = ~ weight)
+
+    ## Gold standard
+    props <- c(0.2, 0.4, 0.6)
+    vars  <- c(0.2*0.8, 0.4*0.6, 0.6*0.4)
+
+    g1g2 <- abs(props[1] - props[2]) / sqrt((vars[1] + vars[2]) / 2)
+    g2g3 <- abs(props[2] - props[3]) / sqrt((vars[2] + vars[3]) / 2)
+    g3g1 <- abs(props[3] - props[1]) / sqrt((vars[3] + vars[1]) / 2)
+
+
+    ## Expectations
+    ## Binary case
+    expect_equal(StdDiff(variable, group, binary = TRUE),
+                 c(g1g2, g3g1, g2g3))
+    ## Continuous case
+    expect_equal(StdDiff(variable, group, binary = FALSE),
+                 as.vector(StdDiffOld(variable, group)))
+
+    ## Weighted
+    ## Binary case
+    expect_equal(svyStdDiff("variable","group", design = data1Svy, binary = TRUE),
+                 c(g1g2, g3g1, g2g3))
+    ## Continuous case
+    expect_equal(svyStdDiff("variable","group", design = data1Svy, binary = FALSE),
+                 as.vector(svyStdDiffOld("variable","group", design = data1Svy)))
+
+
+    ## Multiple variables
+    expect_equal(StdDiffs(data = data1, vars = c("variable","variable"),
+                          groupVar = "group", binaryVars = "variable"),
+                 list(c(g1g2, g3g1, g2g3), c(g1g2, g3g1, g2g3)))
+
+    expect_equal(StdDiffs(data = data1, vars = c("variable","variable"),
+                          groupVar = "group", binaryVars = NA),
+                 list(as.vector(StdDiffOld(variable, group)),
+                      as.vector(StdDiffOld(variable, group))))
+
+    expect_equal(svyStdDiffs(data = data1Svy, vars = c("variable","variable"),
+                          groupVar = "group", binaryVars = "variable"),
+                 list(c(g1g2, g3g1, g2g3), c(g1g2, g3g1, g2g3)))
+
+    expect_equal(svyStdDiffs(data = data1Svy, vars = c("variable","variable"),
+                          groupVar = "group", binaryVars = NA),
+                 list(as.vector(svyStdDiffOld("variable","group", design = data1Svy)),
+                      as.vector(svyStdDiffOld("variable","group", design = data1Svy))))
+})
+
+test_that("Multinomial SMD is correct", {
+
+    set.seed(102)
+    ## 4-category variable
+    probs <- c(0.1,0.3,0.5,0.2)
+    X <- rmultinom(n = 100, size = 1, prob = probs)
+    X <- as.vector(seq_along(probs) %*% X)
+    ## Three groups
+    group <- c(rep(1,20), rep(2,30), rep(3,50))
+
+    ## Drop first column to avoid dependent column
+    ## The result does not change if generalized inverse is used
+    dummyX <- dummies::dummy(X)[, -1, drop = FALSE]
+
+    lstDummyX <- split(x = as.data.frame(dummyX), f = group, drop = FALSE)
+
+    ## Means for each indicator
+    means <- lapply(lstDummyX, MultinomialMeans)
+    ## cov mat for each indicators
+    covs  <- lapply(means, MultinomialVar)
+
+    meanDiff1 <- means[[1]] - means[[2]]
+    meanDiff2 <- means[[1]] - means[[3]]
+    meanDiff3 <- means[[2]] - means[[3]]
+
+    covMean1 <- (covs[[1]] + covs[[2]]) / 2
+    covMean2 <- (covs[[1]] + covs[[3]]) / 2
+    covMean3 <- (covs[[2]] + covs[[3]]) / 2
+
+    smd1 <- drop(sqrt(t(meanDiff1) %*% MASS::ginv(covMean1) %*% t(t(meanDiff1))))
+    smd2 <- drop(sqrt(t(meanDiff2) %*% MASS::ginv(covMean2) %*% t(t(meanDiff2))))
+    smd3 <- drop(sqrt(t(meanDiff3) %*% MASS::ginv(covMean3) %*% t(t(meanDiff3))))
+
+    ## Calculate using multi-category to SMD function
+    outSmd <- StdDiffMulti(variable = X, group = group)
+
+    expect_equal(length(outSmd), 3)
+    expect_equal(outSmd, c(smd1,smd2,smd3))
+
+
+    ##
+    ## Repeat for dummy variable matrix with all columns (k-1 independent)
+    dummyX <- dummies::dummy(X)
+    lstDummyX <- split(x = as.data.frame(dummyX), f = group, drop = FALSE)
+    ## Means for each indicator
+    means <- lapply(lstDummyX, MultinomialMeans)
+    ## cov mat for each indicators
+    covs  <- lapply(means, MultinomialVar)
+    meanDiff1 <- means[[1]] - means[[2]]
+    meanDiff2 <- means[[1]] - means[[3]]
+    meanDiff3 <- means[[2]] - means[[3]]
+    covMean1 <- (covs[[1]] + covs[[2]]) / 2
+    covMean2 <- (covs[[1]] + covs[[3]]) / 2
+    covMean3 <- (covs[[2]] + covs[[3]]) / 2
+    smd1 <- drop(sqrt(t(meanDiff1) %*% MASS::ginv(covMean1) %*% t(t(meanDiff1))))
+    smd2 <- drop(sqrt(t(meanDiff2) %*% MASS::ginv(covMean2) %*% t(t(meanDiff2))))
+    smd3 <- drop(sqrt(t(meanDiff3) %*% MASS::ginv(covMean3) %*% t(t(meanDiff3))))
+
+    expect_equal(outSmd, c(smd1,smd2,smd3))
+
+
+    ## Check behavior with a binary variable
+    set.seed(102)
+    ## Binary variable
+    X <- rbinom(n = 100, size = 1, prob = 0.2)
+    group <- rep(c(0,1), c(20,80))
+
+    dummyX <- dummies::dummy(X)[, -1, drop = FALSE]
+
+    lstDummyX <- split(x = as.data.frame(dummyX), f = group, drop = FALSE)
+
+    ## Means for each indicator
+    means <- lapply(lstDummyX, MultinomialMeans)
+    ## cov mat for each indicators
+    covs  <- lapply(means, MultinomialVar)
+    ##
+    meanDiff1 <- means[[1]] - means[[2]]
+    covMean1 <- (covs[[1]] + covs[[2]]) / 2
+
+    smd1 <- drop(sqrt(t(meanDiff1) %*% MASS::ginv(covMean1) %*% t(t(meanDiff1))))
+
+    ## Comparison to manual calculation
+    expect_equal(StdDiffMulti(variable = X, group = group), smd1)
+    ## Comparison to continuous version with binary support
+    expect_equal(StdDiffMulti(variable = X, group = group),
+                 StdDiff(variable = X, group = group, binary = TRUE))
+
+    ## Check missing value handling (binary)
+    X[13] <- NA
+    ## within group values
+    means <- tapply(X, group, mean, na.rm = TRUE)
+    covs  <- means * (1 - means)
+    ## Diff and cov mean
+    meanDiff1 <- means[[1]] - means[[2]]
+    covMean1  <- (covs[[1]] + covs[[2]]) / 2
+    smd1 <- drop(sqrt(t(meanDiff1) %*% MASS::ginv(covMean1) %*% t(t(meanDiff1))))
+    smd2 <- meanDiff1 / sqrt(covMean1)
+
+    expect_equal(smd1, smd2)
+
+    expect_equal(StdDiff(variable = X, group = group, binary = TRUE), smd1)
+    expect_equal(StdDiff(variable = X, group = group, binary = TRUE), smd2)
+
+    expect_equal(StdDiffMulti(variable = X, group = group),
+                 StdDiff(variable = X, group = group, binary = TRUE))
+
+    expect_equal(StdDiffMulti(variable = X, group = group),
+                 StdDiff(variable = X, group = group, binary = TRUE))
+
+})
+
+
+test_that("mutlinomial SMD for survey data is correct", {
+
+    set.seed(102)
+    ## 4-category variable
+    probs <- c(0.1,0.3,0.5,0.2)
+    X <- rmultinom(n = 100, size = 1, prob = probs)
+    X <- as.vector(seq_along(probs) %*% X)
+    ## Three groups
+    group <- c(rep(1,20), rep(2,30), rep(3,50))
+
+    dat <- data.frame(X = X, group = group, weight = 1)
+    datSvy <- svydesign(ids = ~ 1, data = dat, weights = ~ weight)
+
+    ## Check tables match with weight of 1 for everyone
+    tab1svytable <- svytable(~ group + X, design = datSvy)
+    tab1xtabs <- xtabs(~ group + X, data = dat)
+    expect_true(all(tab1svytable == tab1xtabs))
+
+    ## Check equality for equal weight case
+    expect_equal(svyStdDiffMulti("X", "group", datSvy),
+                 StdDiffMulti(dat$X, group = dat$group))
+
+    ## Missing value is introduced
+    dat$X[13] <- NA
+    ## Recreate
+    datSvy <- svydesign(ids = ~ 1, data = dat, weights = ~ weight)
+    ## Tables again
+    tab1svytable <- svytable(~ group + X, design = datSvy)
+    tab1xtabs <- xtabs(~ group + X, data = dat)
+
+    expect_true(all(tab1svytable == tab1xtabs))
+
+
+    expect_equal(svyStdDiffMulti("X", "group", datSvy),
+                 StdDiffMulti(dat$X, group = dat$group))
+
+})
