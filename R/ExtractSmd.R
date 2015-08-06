ff9d23eef07d62b294b559dbe50cae5207df16f2
kaz-yos <kazukiyoshida@mail.harvard.edu>
2015-08-06 16:09:11 -0400

    Add skeletons for ExtractSmd and tests

---
+++ R/ExtractSmd.R
@@ -0,0 +1,16 @@
+##' Extract standardized mean differences from a (svy)TableOne object
+##'
+##' Extracts standardized mean differences data as a matrix from a (svy)TableOne object
+##'
+##'
+##' @param x A stratified (svy)TableOne object containing standardized mean differences.
+##' @return A matrix containing the average standardized mean differences as well as the all possible pairwise standardized mean differences. Variables are ordered in the same order as the printed table.
+##' @author Kazuki Yoshida
+##' @examples
+##'
+##' ##
+##'
+##' @export
+ExtractSmd <- function(x) {
+    NA
+}
