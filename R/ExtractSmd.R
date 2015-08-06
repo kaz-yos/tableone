141d7279e2b8fa305f02772f672e8f3fed4da410
kaz-yos <kazukiyoshida@mail.harvard.edu>
2015-08-06 17:19:14 -0400

    Add better documentation for ExtractSmd() (vector can return)

--- R/ExtractSmd.R
+++ R/ExtractSmd.R
@@ -1,11 +1,13 @@
 ##' Extract standardized mean differences from a (svy)TableOne object
 ##'
-##' Extracts standardized mean differences data as a matrix from a (svy)TableOne object
+##' Extracts standardized mean differences data as a vector or matrix from a (svy)TableOne object
 ##'
 ##'
 ##' @param x A stratified (svy)TableOne object containing standardized mean differences.
-##' @return A matrix containing the average standardized mean differences as well as the all possible pairwise standardized mean differences. Variables are ordered in the same order as the printed table.
+##' @return A vector or matrix containing the average standardized mean differences (if more than two contrasts exist) as well as the all possible pairwise standardized mean differences. Variables are ordered in the same order as the printed table.
 ##' @author Kazuki Yoshida
+##' @seealso
+##' \code{\link{CreateTableOne}}, \code{\link{CreateTableOne}}
 ##' @examples
 ##'
 ##' ## See examples for CreateTableOne and svyCreateTableOne
