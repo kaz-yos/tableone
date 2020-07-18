8eaa68ec10276ebb107b3ea52243ce73152edb6d
Alexander Bartel <github@ndevln.org>
2020-07-18 14:56:29 +0200

    Check if lmerTest is installed

--- R/lmerTest_utils.R
+++ R/lmerTest_utils.R
@@ -1,12 +1,16 @@
 ## Minimized version of the helper function provided by lmerTest authors.
 lmerTest_summary <- function(object, ...) {
-    if (inherits(object, "lmerModLmerTest")) {
-        ## lmerTest object
+    if (requireNamespace("lmerTest", quietly = TRUE)) {
+        if (inherits(object, "lmerModLmerTest")) {
+            ## lmerTest object
+            return(summary(object, ...))
+        } else {
+            ## lme4 object
+            return(summary(lmerTest::as_lmerModLmerTest(object), ...))
+        }
+        ## *merModLmerTest objects and/or 'lmerTest' is not available
         return(summary(object, ...))
     } else {
-        ## lme4 object
-        return(summary(lmerTest::as_lmerModLmerTest(object), ...))
+        stop("ShowRegTable: Please install the package \"lmerTest\":\n install.package('lmerTest')", call. = F)
     }
-    ## *merModLmerTest objects and/or 'lmerTest' is not available
-    return(summary(object, ...))
 }
